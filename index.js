1cdd5af03ce9cb560a80f82e782e4112dac01d21
bsorrentino <bartolomeo.sorrentino@gmail.com>
2015-02-18 18:54:26 +0100

    update

--- index.js
+++ index.js
@@ -7,4 +7,28 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+
+  getIPv4interface();
+
+});
+
+
+function getNetworkInterfaces() {
+  var os = require('os');
+  var util = require("util");
+
+  var ifaces = os.networkInterfaces();
+
+  Object.keys(ifaces).forEach(function (ifname) {
+
+    ifaces[ifname].forEach(function (iface) {
+      console.log( ifname ,":", util.inspect(iface));
+
+      if( iface.family==='IPv4' && !iface.internal)
+        console.log( "listen to ", "http://" + iface.address + ":3000"  );
+
+    });
+  });
+
+}
